b52787bc02d8193d4935d2a800b4a2325f3dece1
Julian <julian4suarez@gmail.com>
2016-12-09 18:01:29 +0100

    using all leaf types

--- mainProgram.m
+++ mainProgram.m
@@ -1,24 +1,31 @@
 % create repo of images
-categories = {'with', 'without', 'maybe'};
-imds = imageDatastore(fullfile('data/testData' , categories), 'LabelSource', 'foldernames');
-
+categories = genvarname(repmat({'leaf'}, 1, 15), 'leaf');
+imds = imageDatastore(fullfile('data/' , categories), 'LabelSource', 'foldernames');
 tbl = countEachLabel(imds);
-[trainingSet, validationSet] = splitEachLabel(imds, 0.3, 'randomize');
 
-% vag of features
-%bag = bagOfFeatures(imds, 'Verbose', false);
+[trainingSet, ~] = splitEachLabel(imds, 0.01, 'randomize');
+[validationSet, ~] = splitEachLabel(imds, 0.03, 'randomize');
+
+disp([num2str(length(trainingSet.Files)) ' images for training']);
+disp([num2str(length(validationSet.Files)) ' images for Validation']);
     
+disp('extracting features of training set...');
 dataTrain = extractMatrixFeatures(trainingSet);
+
+disp('training model ...');
 t = fitctree(dataTrain, cellstr(trainingSet.Labels));
 %t = fitctree(data, cellstr(trainingSet.Labels),'PredictorNames',{'SL' 'SW' });
 
+disp('extracting features of validation set...');
 dataValid = extractMatrixFeatures(validationSet);
 
+disp('predicting ...');
 result = predict(t, dataValid);
 
 % simple accurracy
-
 validResult = cellstr(validationSet.Labels);
 hits = sum (strcmp(result, validResult));
-hits / length(validResult)
+
+fprintf('acurracy : %1.4f \n', hits / length(validResult))
+
 
