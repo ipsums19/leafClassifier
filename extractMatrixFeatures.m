9377b3bf78426754b819d4b7a28b57692f6f077b
Joan Barba <joanbarba19@gmail.com>
2016-12-12 13:08:15 +0100

    simmetry

--- extractMatrixFeatures.m
+++ extractMatrixFeatures.m
@@ -1,8 +1,8 @@
 function [setFeatures] = extractMatrixFeatures(imgData)
     
     nFiles = length(imgData.Files);
-    nFeatures = [1024 512 1 1];
-    setFeatures = zeros(nFiles , sum(nFeatures));
+    nFeatures = [1024 512 1 1 1];
+    setFeatures = zeros(nFiles , nFeatures);
     
     for i = 1:nFiles
         % Image Info
@@ -25,13 +25,13 @@ function [setFeatures] = extractMatrixFeatures(imgData)
         BW = Itmp < 180;
         setFeatures(i, (sum(nFeatures(1:1))+1):sum(nFeatures(1:2))) = sum(BW)';
         
-        % stem
-%         SE1 = strel('disk', 10);
-%         SE2 = strel('disk', 6);
-%         E = imopen(BW, SE1);
-%         E = BW - E;
-%         E = imopen(E, SE2);
-%         setFeatures(i, (sum(nFeatures(1:2))+1):sum(nFeatures(1:3))) = sum(sum(E));
+        %stem
+        SE1 = strel('disk', 10);
+        SE2 = strel('disk', 6);
+        E = imopen(BW, SE1);
+        E = BW - E;
+        E = imopen(E, SE2);
+        setFeatures(i, (sum(nFeatures(1:2))+1):sum(nFeatures(1:3))) = sum(sum(E));
 %         
 %         % serrated edge
 %         SE = strel('disk', 100);
@@ -42,6 +42,24 @@ function [setFeatures] = extractMatrixFeatures(imgData)
 %         IM = bwmorph(IM, 'clean');
 %         C = bwconncomp(IM);
 %         setFeatures(i, (sum(nFeatures(1:3))+1):sum(nFeatures(1:4))) = C.NumObjects;
+
+        % simmetry
+%         BW = imfill(BW, 'holes');
+%         h = 1024;
+%         w = 514;
+%         R = imresize(BW, [h w]);
+% 
+%         mid = h/2;
+%         mid1 = sum(sum(R(1:mid,:)));
+%         mid2 = sum(sum(R(mid:h,:)));
+%         diff = abs(mid1 - mid2);
+%         setFeatures(i, (sum(nFeatures(1:3))+1):sum(nFeatures(1:4))) = diff;
+%         
+%         mid = w/2;
+%         mid1 = sum(sum(R(:,1:mid)));
+%         mid2 = sum(sum(R(:,mid:w)));
+%         diff = abs(mid1 - mid2);
+%         setFeatures(i, (sum(nFeatures(1:4))+1):sum(nFeatures(1:5))) = diff;
 
     end  
 end
--- extractingCategories.m
+++ extractingCategories.m
@@ -1,21 +1,17 @@
 close all
-I = rgb2gray(imread('data/leaf1/l1nr045.tif'));
-imshow(I);
-BW = I < 180;
-%F = imfill(BW, 'holes');
-%imshow(BW);
-
-% erocionamos
-SE = strel('disk', 100);
-E = imopen(BW, SE);
-
-% contar dientes
-IM = BW - E; figure; %imshow(IM);
-SE = strel('disk', 2);
-IM = imerode(IM, SE); %imshow(IM);
-%IM = bwmorph(IM, 'clean'); figure; imshow(IM);
-C = bwconncomp(IM);
-numObjects = C.NumObjects
-%imshow(E);
-%figure;
+I = rgb2gray(imread('data/leaf1/l1nr011.tif'));
 %imshow(I);
+BW = I < 180;
+BW = imfill(BW, 'holes');
+[height, width] = size(BW);
+ratio = height / width;
+h = 1024;
+w = 514;
+R = imresize(BW, [h w]);
+
+mid = w/2;
+mid1 = sum(sum(R(:,1:mid)));
+mid2 = sum(sum(R(:,mid:w)));
+diff = abs(mid1 - mid2);
+imshow(R(:,mid:w));
+%imshow(BW);
