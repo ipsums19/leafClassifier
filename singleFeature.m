4d450c33e99a06ce3caf615ce31325c61c3013bf
Julian <julian4suarez@gmail.com>
2016-12-18 02:19:37 +0100

    shape context p1

--- singleFeature.m
+++ singleFeature.m
@@ -1,24 +1,92 @@
-O = imread('data/paloTest/with/l2nr001.tif');
-I = rgb2gray(O);
+%read image
+O = imread('data/leaf15/l15nr017.tif');
+I = imresize(rgb2gray(O), 1);
+% binarization
+BW = I > 180;
+[rows, cols] = size(BW);
 
-
-BW = 1 - imbinarize(I);
+% get shape
+BW = bwmorph(BW, 'remove') ;
+% remove margin shape
+mg = 10;
+BW(1:mg, :) = 0;
+BW((rows-mg):rows, :) = 0;
+BW(:, 1:mg) = 0;
+BW(:, (cols-mg):cols) = 0;
+% get leaf shape
+BW = bwareafilt(BW, 1);
+% get props
+CC = bwconncomp(BW);
+P = regionprops(CC, 'area', 'BoundingBox');
+BB = P.BoundingBox;
 
 imshow(BW);
- SE1 = strel('disk', 10);
-        SE2 = strel('disk', 6);
-        E = imopen(BW, SE1);
-        E = BW - E;
-        E = imopen(E, SE2);
-        figure; imshow(E);
+rectangle('Position', [BB(1),BB(2),BB(3),BB(4)], 'EdgeColor','r','LineWidth',2 );
 
-CC = bwconncomp(BW);
-Props = regionprops(CC, 'Area', 'Centroid', 'Extent', 'Eccentricity', 'Solidity');
-[A , leaf] = max([Props.Area]);
-
-Centroid = Props(leaf).Centroid;
-posx = Centroid(1);
-posy = Centroid(2);
-
+% starting position
+posx = int16(BB(2));
+[ x, posy] = max(BW(posx, :)) ;
 line([posy-20,posy+20], [posx, posx]);
 line([posy, posy], [posx-20,posx+20]);
+BW(posx:(posx+10), (posy-1)) = 0;
+BW(posx, posy) = 0;
+% lists of point
+V = BW; 
+imshow(V);
+listTest = zeros(P.Area, 2);
+import java.util.LinkedList
+q = LinkedList();
+q.add(posx);
+q.add(posy);
+i = 1;
+while  ~ q.isEmpty()
+    px = q.remove();
+    py = q.remove();   
+    listTest(i, 1) = px;
+    listTest(i, 2) = py;
+    i = i+1 ;
+    % harcode neightbours 
+    z = [1, 0 ; 1 ,1 ; 0 ,1 ; -1, 0 ; -1 ,-1 ; 0 ,-1 ; 1, -1; -1, 1];
+    for j = 1:8
+        npx = px + z(j, 1);
+        npy = py + z(j, 2);
+        if V(npx , npy) > 0
+            V(npx, npy) = 0;
+            q.add(npx);
+            q.add(npy);
+        end
+    end     
+    
+end
+
+% list of 1024 point
+nPoints = 1024;
+offset = P.Area / nPoints;
+points = zeros(nPoints,2);
+for i = 1:(nPoints)
+    indx = int16(i*offset);
+    points(i, :) = listTest(indx, :);
+end
+
+% normalize
+mins = min(points);
+maxs = max(points);
+points = [ points(:, 2) - mins(2), abs(points(:, 1) - maxs(1))];
+maxs = max(points);
+points = [points(:, 1)/maxs(1) , points(:, 2)/maxs(2)];
+
+% points
+binEdges = logspace(log10(1/2^5),0,5);
+% log polar point
+for i = 1:nPoints
+    point = points(i, :);
+    [theta, rho] = cart2pol(points(:, 1)-point(1), points(:, 2)-point(2));
+    theta = floor(theta / (pi / 6) + 6) + 1;
+    for k = 1:5
+        rho(rho < binEdges(k)) = k;
+    end    
+	accumarray([theta(:), rho(:)], 1);   
+end
+
+
+
