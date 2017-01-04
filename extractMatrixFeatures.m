4ff0502c9a88a1b72809a1b8d1d67cf983618125
Julian Suarez <julian4suarez@gmail.com>
2017-01-04 17:21:55 +0100

    test

--- extractMatrixFeatures.m
+++ extractMatrixFeatures.m
@@ -1,46 +1,50 @@
 function [setFeatures] = extractMatrixFeatures(imgData)
     
     nFiles = length(imgData.Files);
-    nFeatures = [1024 512 1 1 1 1];
+    nFeatures = [1024 512 1 1 1 1 1 1 1];
     setFeatures = zeros(nFiles , sum(nFeatures));
     
     for i = 1:nFiles
         % Image Info
         data = readimage(imgData,i);
         I = rgb2gray(data);
+        BW = I < 180;
         [height, width] = size(I);
         ratio = height / width;
         
         % cumulated h     
         h = 1024;
         w = h / ratio;
-        Itmp = imresize(I, [h w]);
-        BW = Itmp < 180;
-        setFeatures(i, 1:nFeatures(1)) = sum(BW, 2)';
+        Itmp = imresize(BW, [h w]);
+        setFeatures(i, 1:nFeatures(1)) = sum(Itmp, 2)';
         
         % cumulated v    
         w = 512;
         h = w * ratio;
-        Itmp = imresize(I, [h w]);
-        BW = Itmp < 180;
-        setFeatures(i, (sum(nFeatures(1:1))+1):sum(nFeatures(1:2))) = sum(BW)';
+        Itmp = imresize(BW, [h w]);
+        setFeatures(i, (sum(nFeatures(1:1))+1):sum(nFeatures(1:2))) = sum(Itmp)';
         
         %stem
+        BW = imresize(BW, [h w]);
         SE1 = strel('disk', 10);
-        SE2 = strel('disk', 6);
-        E = imopen(BW, SE1);
-        E = BW - E;
-        E = imopen(E, SE2);
-        setFeatures(i, (sum(nFeatures(1:2))+1):sum(nFeatures(1:3))) = sum(sum(E));
+        ILeaf = imopen(BW, SE1);
+        ILeaf = bwareafilt(ILeaf, 1);
+        IStem = bwareafilt(logical(BW-ILeaf), 1); 
+        Itmp = imresize(IStem, [h w]);
+        setFeatures(i, (sum(nFeatures(1:2))+1):sum(nFeatures(1:3))) = sum(sum(Itmp));
 
-        %'Eccentricity', 'Solidity', 'Extent'
+        %'Eccentricity', 'Solidity', 'Extent'   
         CC = bwconncomp(BW);
-        Props = regionprops(CC, 'Area', 'Extent', 'Eccentricity', 'Solidity');
-        X = cell2mat(struct2cell(Props))';
-        [x y] = max(X(:,1));
-        setFeatures(i, (sum(nFeatures(1:3))+1):sum(nFeatures(1:4))) = X(y,2);
-        setFeatures(i, (sum(nFeatures(1:4))+1):sum(nFeatures(1:5))) = X(y,3);
-        setFeatures(i, (sum(nFeatures(1:5))+1):sum(nFeatures(1:6))) = X(y,4);
+        P = regionprops(CC, 'Extent', 'Eccentricity', 'Solidity');
+        setFeatures(i, sum(nFeatures(1:4))) = P.Extent;
+        setFeatures(i, sum(nFeatures(1:5))) = P.Eccentricity;
+        setFeatures(i, sum(nFeatures(1:6))) = P.Solidity;     
+        
+        CC = bwconncomp(ILeaf);
+        P = regionprops(CC, 'Extent', 'Eccentricity', 'Solidity');
+        setFeatures(i, sum(nFeatures(1:7))) = P.Extent;
+        setFeatures(i, sum(nFeatures(1:8))) = P.Eccentricity;
+        setFeatures(i, sum(nFeatures(1:9))) = P.Solidity;  
 
 
     end  
